81686775ad83128024794d64f0b357f312cef6ff
Maicon Alves <AD648ZZ@mmm.com>
2023-06-06 11:24:52 -0300

    Profile avatar and banner created, add data

--- components/User/Profile.jsx
+++ components/User/Profile.jsx
@@ -1,10 +1,19 @@
 import { Card } from 'primereact/card';
-//import style from '@components/User/perfil.module.css'
+import styles from '../User/perfil.module.css';
+import { Avatar } from 'primereact/avatar';
+import Image from 'next/image';
+import banner from '@/public/user/banner.jpg';
+import profile from '@/public/user/profile.jpg'
 export default function Profile(){
 
     const header = (
         <div>
-                        
+            <div className={styles.container}>
+                <Image src={banner}/>
+            </div>
+            <div className={styles.profile}>
+                <Image src={profile} className={styles.profileImage}/>
+            </div>  
         </div>
     )
 
@@ -13,7 +22,17 @@ export default function Profile(){
             <div className='col-8'>
                 <Card title={header}>
                     <div>
-
+                        <div className='flex flex-column mt-6 ml-6'>
+                            <p className={styles.userData}>
+                                Maicon Alves
+                            </p>
+                            <p className={styles.userSubtitle}>
+                                Engenherio de Software | Javascript | React | React Native | Nodejs
+                            </p>
+                            <p className={styles.userLocation}>
+                                Nova Odessa - SP | Brasil
+                            </p>
+                        </div>
                     </div>
                 </Card>
             </div>
