b94da71118f02a028f4be0e3d7f1371652eefbd9
Maicon Alves <AD648ZZ@mmm.com>
2023-08-02 10:45:34 -0300

    Created functions for create new vacancys

--- components/Vagas/List.jsx
+++ components/Vagas/List.jsx
@@ -1,7 +1,10 @@
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
-import styles from './vagas.module.css';
 import { Divider } from 'primereact/divider';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { useState } from "react";
+import styles from './vagas.module.css';
 export default function List(){
     
     const header = (
@@ -14,7 +17,67 @@ export default function List(){
             </div>
         </div>
     )
+    const [visible, setVisible] = useState(false);
+    const [vagaTitle, setvagaTitle] = useState('');
+    const [vagaCompany, setVagaCompany] = useState('');
+    const [vagaLocation, setVagaLocation] = useState('');
 
+    const footerContent = (
+        <div>
+            <Button label="Cancelar" icon="pi pi-times" onClick={() => setVisible(false)} className="p-button-text" />
+            <Button label="Concluído" icon="pi pi-check" onClick={() => createVagas()} autoFocus />
+        </div>
+    );
+
+    const [vagas, setVagas] = useState([
+        {
+            title:'Programador(a) Fullstack Junior (PCD) (Home office)',
+            company:'PCD-IN',
+            location:'Americana - SP'
+        },
+        {
+            title:'Programador(a) Fullstack Pleno (PCD) (Home office)',
+            company:'PCD-IN',
+            location:'Americana - SP'
+        },
+        {
+            title:'Programador(a) Fullstack Sênior (PCD) (Home office)',
+            company:'PCD-IN',
+            location:'Americana - SP'
+        }
+    ])
+
+    function getVagas(){
+        return vagas.map((vaga, index) =>(
+                <div key={index} className='mt-4'>
+                    <div className='flex flex-column'>
+                        <div className={styles.vagaTitle}>
+                            {vaga.title}
+                        </div>
+                        <div>{vaga.company}</div>
+                        <div className={styles.vagaCity}>{vaga.location}</div>
+                    </div>
+                    {index !== vagas.length - 1 && <Divider />}
+                </div>
+            )
+        )
+    }
+
+    function createVagas(){
+        const newVaga = {
+            title:vagaTitle,
+            company:vagaCompany,
+            location:vagaLocation
+        }
+        setVagas([...vagas, newVaga])
+        setVisible(false)
+    }
+    function openDialogCreateNewVacancy(){
+        setvagaTitle('');
+        setVagaCompany('');
+        setVagaLocation('');
+        setVisible(true)
+    }
 
     return(
         <div className='grid'>
@@ -31,7 +94,19 @@ export default function List(){
                             <Button label="Minhas candidaturas" text icon="pi pi-file" severity="secondary"/>
                         </div>
                         <div className='flex justify-content-center mt-2'>
-                            <Button label="Anunciar vaga" outlined className='w-12'/>
+                            <Button label="Anunciar vaga" outlined className='w-12' onClick={() => openDialogCreateNewVacancy()} />
+                            <Dialog header="Anunciar vagas" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)} footer={footerContent}>
+                                <div className='flex alling-items-centet justify-content-center'>
+                                    <div className='w-12'>
+                                        <p>Título da vaga</p>
+                                        <InputText className='w-12' value={vagaTitle} onChange={(e) => setvagaTitle(e.target.value)}/>
+                                        <p>Empresa contratante</p>
+                                        <InputText className='w-12' value={vagaCompany} onChange={(e) => setVagaCompany(e.target.value)} />
+                                        <p>Local de trabalho</p>
+                                        <InputText className='w-12' value={vagaLocation} onChange={(e) => setVagaLocation(e.target.value)}/>
+                                    </div>
+                                </div>
+                            </Dialog>
                         </div>
 
                     </div>
@@ -39,38 +114,7 @@ export default function List(){
             </div>
             <div className='col-9'>
                 <Card title={header}>
-                    <div className='flex flex-column'>
-                        <div className='mt-4'>
-                            <div className='flex flex-column'>
-                                <div className={styles.vagaTitle}>
-                                    Programador(a) Fullstack Sênior (PCD) (Presencial)
-                                </div>
-                                <div>PCD-IN</div>
-                                <div className={styles.vagaCity}>Americana - SP</div>
-                            </div>
-                        </div>
-                        <Divider />
-                        <div>
-                            <div className='flex flex-column'>
-                                <div className={styles.vagaTitle}>
-                                    Programador(a) backend Pleno (PCD) (Híbrido)
-                                </div>
-                                <div>PCD-IN</div>
-                                <div className={styles.vagaCity}>Americana - SP</div>
-                            </div>
-                        </div>
-                        <Divider />
-                        <div>
-                            <div className='flex flex-column'>
-                                <div className={styles.vagaTitle}>
-                                    Programador(a) Fullstack Sênior (PCD) (Presencial)
-                                </div>
-                                <div>PCD-IN</div>
-                                <div className={styles.vagaCity}>Americana - SP</div>
-                            </div>
-                        </div>
-                        <Divider />
-                    </div>
+                    {getVagas()}
                 </Card>
             </div>
         </div>
