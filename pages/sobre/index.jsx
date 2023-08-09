9fa73957007c78d2d97f0a2ad09a02c61fef1b1b
Leonardo Marcondeli <leonardo.marcondeli@gmail.com>
2023-08-08 21:24:33 -0300

    Create about page

---
+++ pages/sobre/index.jsx
@@ -0,0 +1,15 @@
+import CardAbout from '../../components/CardAbout/Card'
+import SpliterAbout from '../../components/SplitterAbout/Spliter'
+
+export default function pageAbout (){
+    return(
+        <>
+            <CardAbout/>
+            <div className='mt-2'>
+                <SpliterAbout/>
+            </div>
+        </>
+    )
+}
+
+
