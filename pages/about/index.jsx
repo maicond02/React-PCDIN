28146b100397a208a7bb6568ed3e5ffb1d2554c4
Leonardo Marcondeli <leonardo.marcondeli@gmail.com>
2023-08-14 22:10:01 -0300

    UpdateImageAbout

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -2,15 +2,20 @@ import CardAbout from '../../components/About/Card'
 import SpliterAbout from '../../components/About/Spliter'
 import TimelineAbout from '../../components/TimelineAbout/TimelineAbout'
 import FieldsetAbout from '../../components/FieldsetAbout/FieldsetAbout'
+import curiosity from '../../public/about/curiosity.gif'
+import Image from 'next/image';
 
 export default function pageAbout (){
     return(
         <>
-            <div className='mt-1'>
-                <FieldsetAbout/>
+            <div className='flex align-items-center justify-content-center mt-2'> 
+                <Image  src={curiosity} alt="teste" width={600} height={500}/>
             </div>
             <div className='mt-2'>
                 <TimelineAbout/>
+            </div>
+            <div className='mt-2'>
+                <FieldsetAbout/>
             </div>
         </>
     )
