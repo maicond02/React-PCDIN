4720c135f3954f019457611f83c91c6239538817
LeonardoMarcondeli <leonardo.marcondeli@gmail.com>
2023-08-27 13:51:46 -0300

    page update on creating the support page

---
+++ pages/suport/index.jsx
@@ -0,0 +1,15 @@
+import CardAbout from '../../components/About/Card'
+import TimelineAbout from '../../components/TimelineAbout/TimelineAbout'
+import FieldsetAbout from '../../components/FieldsetAbout/FieldsetAbout'
+import curiosity from '../../public/about/curiosity.gif'
+import Image from 'next/image';
+
+export default function pageAbout (){
+    return(
+        <>
+            <p>Hello</p>
+        </>
+    )
+}
+
+
